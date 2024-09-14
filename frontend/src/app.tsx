619316443d3a1555e31495ccf1a9c7dd52e0d074
Samuelfjr <devsamuelfjr@gmail.com>
2024-09-14 20:08:36 -0300

    refactor: empty screen button

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,6 +2,7 @@ import { Plus } from 'lucide-react'
 
 import logo from "./assets/logo-in-orbit.svg"
 import letsStart from './assets/lets-start-illustration.svg'
+import { Button } from './components/ui/ui/button'
 
 export function App() {
   return (
@@ -10,10 +11,10 @@ export function App() {
     <img src={letsStart} alt="lets start" />
     <p className="leading-relaxed text-center text-zinc-300 max-w-80">Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?</p>
 
-    <button className='px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tighter hover:bg-violet-600'>
+    <Button>
       <Plus className='size-4'/>
       Cadastrar meta      
-    </button>
+    </Button>
    </div>
   )
 }
