0ed708b53b278c935a0fc9906989f0200a692922
Samuelfjr <devsamuelfjr@gmail.com>
2024-09-14 19:35:34 -0300

    feat: partial empty screen

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,7 +1,15 @@
+import logo from "./assets/logo-in-orbit.svg"
+import letsStart from './assets/lets-start-illustration.svg'
 export function App() {
   return (
-    <>
-        <h1 className="text-white bg-black text-xl/">NLW Pocket</h1>
-    </>
+   <div className="flex flex-col items-center justify-center h-screen gap-8">
+    <img src={logo} alt="in.orbit" />
+    <img src={letsStart} alt="lets start" />
+    <p className="leading-relaxed text-center text-zinc-300 max-w-80">Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?</p>
+
+    <button>
+      Cadastrar meta      
+    </button>
+   </div>
   )
 }
