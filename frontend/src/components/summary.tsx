855a2c60db006f65b8db325f9af5e53b92e202a1
Samuelfjr <devsamuelfjr@gmail.com>
2024-09-21 20:23:39 -0300

    feat: uses stale time in use query

--- frontend/src/components/summary.tsx
+++ frontend/src/components/summary.tsx
@@ -5,8 +5,17 @@ import { InOrbitIcon } from './in-orbit-icon'
 import { Progress, ProgressIndicator } from './ui/ui/progress-bar'
 import { Separator } from './ui/ui/separator'
 import { OutlineButton } from './ui/ui/outline-button'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from '../http/get-summary'
 
 export function Summary() {
+  
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 seconds
+  })
+  
   return (
     <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
       <div className="flex items-center justify-between">
